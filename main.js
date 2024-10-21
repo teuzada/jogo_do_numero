d92fc75db5c94382334548db177adb7f3c2d3340
teuzada <163007822+teuzada@users.noreply.github.com>
2024-10-21 11:19:52 -0300

    fsfsdfds

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+    let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    const palpites = document.querySelector('.palpites');
+    const ultimoResultado = document.querySelector('.ultimoResultado');
+    const baixoOuAlto = document.querySelector('.baixoOuAlto');
+    const envioPalpite = document.querySelector('.envioPalpite');
+    const campoPalpite = document.querySelector('.campoPalpite');
+    let contagemPalpites = 1;
+    let botaoReiniciar;
 
+function verificarPalpite() {
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagemPalpites === 1)
+        palpites.textContent = "Palpites anteriores: ";
+}
+
+palpites.textContent += palpiteUsuario + " ";
+
+   if (palpiteUsuario === numeroAleatorio) {
+    ultimoResultado.textContent = "Parabéns! Você Acertou!";
+    ultimoResultado.computedStyleMap.backgroundColor = "green";
+    baixoOuAlto.textContent = "";
+    finalizarJogo(); 
+   }
